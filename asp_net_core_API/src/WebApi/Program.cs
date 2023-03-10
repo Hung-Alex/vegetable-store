11f4cb2dd69548229f5053fc1f7548cf49e08fa8
Hung-Alex <alexanderpham59@gmail.com>
2023-03-11 00:50:59 +0700

    create model user to login jwt

--- asp_net_core_API/src/WebApi/Program.cs
+++ asp_net_core_API/src/WebApi/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using store_vegetable.Data.Context;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore.Design;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreVegetableDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("VegetableStoreDb")));
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ asp_net_core_API/src/store-vegetable.Data/Context/StoreVegetableDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.EntityFrameworkCore;
+using store_vegetable.Core.Entites;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace store_vegetable.Data.Context
+{
+    public  class StoreVegetableDbContext:DbContext
+    {
+        public StoreVegetableDbContext(DbContextOptions<StoreVegetableDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
+
+        }
+    }
+}
