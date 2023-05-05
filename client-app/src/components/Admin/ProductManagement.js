8dc003a3e1dcf8ba39443c6ef061ce04f25d0e81
Quang Huy <quanghuy2707ht@gmail.com>
2023-05-05 15:05:00 +0700

    finish button delete product

--- client-app/src/components/Admin/ProductManagement.js
+++ client-app/src/components/Admin/ProductManagement.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/Admin/product.css";
-import { getFoods } from "../../services/FoodRepository";
+import { deleteFood, getFoods } from "../../services/FoodRepository";
 import Table from "react-bootstrap/Table";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
+import config from "../../config";
 
 const ProductManagement = () => {
     const [foodList, setFoodList] = useState([]);
     const [isVisibleLoading, setIsVisibleLoading] = useState(true);
+
+    const baseUrl = config.i18n.baseUrl.ApiUrl;
+    const token = localStorage.getItem('token')
     // console.log(foodList);
     let p = 1, ps = 10;
 
@@ -20,7 +25,18 @@ const ProductManagement = () => {
                 setFoodList([]);
             // setIsVisibleLoading(false);
         }, [p, ps]);
-    })
+    }, []);
+
+    const handleDelete = (item) => {
+        let form = new FormData();
+
+        deleteFood(form, token).then(() => {
+            let newFoodList = foodList.filter((el) => el.id !== item.id);
+            setFoodList(newFoodList);
+        })
+        // setFoodList(newFoodList);
+    }
+
     return (
         <div className="head-title">
             <h1>Product Management</h1>
@@ -59,7 +75,7 @@ const ProductManagement = () => {
                                     <NavLink to={`/admin/product/edit/${item.id}`}>
                                         <i className="bx bx-edit"></i>
                                     </NavLink>
-                                    <i className="bx bx-trash"></i>
+                                    <i className="bx bx-trash" onClick={() => handleDelete(item)}></i>
                                 </td>
                             </tr>
                         ) :
--- client-app/src/services/FoodRepository.js
+++ client-app/src/services/FoodRepository.js
@@ -2,54 +2,78 @@ import axios from "axios";
 import { get_api, post_api } from "./Methods";
 
 export async function getFoods(pageSize = 10, pageNumber = 1) {
-    try {
-        const response = await axios.get(`https://localhost:7027/api/Foods/?PageSize=${pageSize}&PageNumber=${pageNumber}`);
-        const data = response.data;
-        if (data.isSuccess)
-            return data.result;
-        else
-            return null;
-    } catch (error) {
-        console.log('Error', error.messge);
-        return null;
-    }
+  try {
+    const response = await axios.get(`https://localhost:7027/api/Foods/?PageSize=${pageSize}&PageNumber=${pageNumber}`);
+    const data = response.data;
+    if (data.isSuccess)
+      return data.result;
+    else
+      return null;
+  } catch (error) {
+    console.log('Error', error.messge);
+    return null;
+  }
 }
 
 export async function getFoodById(id = 0) {
-    if (id > 0)
-        return get_api(`https://localhost:7027/api/Foods/${id}`);
-    return null;
+  if (id > 0)
+    return get_api(`https://localhost:7027/api/Foods/${id}`);
+  return null;
 }
 
 export function getCategories(formData) {
-    return get_api("https://localhost:7027/api/categories/", formData);
+  return get_api("https://localhost:7027/api/categories/", formData);
 }
 
-export async function addOrUpdateFood(formData,token, navigate) {
-    try {
-      
-    
-      fetch("https://localhost:7027/api/Foods/", {
-        method: "POST",
-        headers: {
-          Authorization: token,
-        },
-        body: formData,
+export async function addOrUpdateFood(formData, token, navigate) {
+  try {
+    fetch("https://localhost:7027/api/Foods/", {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("add to food successful!");
+        return true;
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          console.log("add to food  successful!");
-          return true;
-        })
-        .catch((error) => {
-          console.error("Error occurred during add to food :", error);
-          return false;
-        });
-  
-      
-    } catch (error) {
-      return false;
-    }
+      .catch((error) => {
+        console.error("Error occurred during add to food :", error);
+        return false;
+      });
+
+
+  } catch (error) {
+    return false;
   }
+}
+
+export async function deleteFood(formData, token) {
+  try {
+    fetch("https://localhost:7027/api/Foods/", {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("add to food successful!");
+        return true;
+      })
+      .catch((error) => {
+        console.error("Error occurred during add to food :", error);
+        return false;
+      });
+
+  } catch (error) {
+    return false;
+  }
+}
